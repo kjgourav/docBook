493772d9c94573c8c8ac367c557aa753137c584f
kjgourav <kjgourav@gmail.com>
2025-02-16 23:33:40 +0530

    admin and pateint register

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,8 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { User } from "../models/userSchema.js";
+import { generateToken } from "../utils/jwtTokens.js";
+
 export const patientRegister = catchAsyncErrors(async (req, res, next) => {
   const {
     firstName,
@@ -41,34 +43,74 @@ export const patientRegister = catchAsyncErrors(async (req, res, next) => {
     aadhaar,
     role
   });
-  res.status(200).json({
-    success: true,
-    message: "user Registered!",
-  });
+  generateToken(user, "user Registered!", 200, res);
 });
 
-export const login = catchAsyncErrors(async (req,res,next) => {
-    const {email , password , confirmPassword, role}=req.body;//role is a static value we will send directly from frontend
-    if(!email || !password || !confirmPassword || !role){
+export const login = catchAsyncErrors(async (req, res, next) => {
+  const { email, password, confirmPassword, role } = req.body; //role is a static value we will send directly from frontend
+  if (!email || !password || !confirmPassword || !role) {
     return next(new ErrorHandler("please provide all details!", 400));
-    }
-    if(password !== confirmPassword){
-        return next(new ErrorHandler("password and confirmPassword does not match!", 400));
+  }
+  if (password !== confirmPassword) {
+    return next(
+      new ErrorHandler("password and confirmPassword does not match!", 400)
+    );
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Password or Email!", 400));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Password Or Email! ", 400));
+  }
+  if (role !== user.role) {
+    return next(new ErrorHandler("User With This Role Not Found", 400));
+  }
+  generateToken(user, "User Logged In Succesfully!", 200, res);
+});
 
-    }
-    const user = await User.findOne({email}).select("+password");
-    if(!user){
-        return next(new ErrorHandler("Invalid Password or Email!", 400))
-    }
-    const isPasswordMatched = await user.comparePassword(password);
-    if(!isPasswordMatched){
-        return next(new ErrorHandler("Invalid Password Or Email! ", 400));
-    }
-    if(role !== user.role){
-        return next(new ErrorHandler("User With This Role Not Found",400));
-    }
-    res.status(200).json({
-        success: true,
-        message: "user Logged In Sucessfully!",
-      });
+export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    password,
+    gender,
+    dob,
+    aadhaar
+  } = req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !password ||
+    !gender ||
+    !dob ||
+    !aadhaar
+  ) {
+    return next(new ErrorHandler("please fill full form!", 400));
+  }
+  const isRegistered = await User.findOne({email});
+  if(isRegistered){
+    return next(new ErrorHandler(`${isRegistered.role} With This Email Already Exists!`)); 
+  }
+  const admin = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    password,
+    gender,
+    dob,
+    aadhaar,
+    role:"Admin",
+  });
+  res.status(200).json({
+    success:true,
+    message:"New Admin Registered!",
+  });
+
 });
