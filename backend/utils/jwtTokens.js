4cfed33aa562c4f9d657ebad509caa6172b4da3e
kjgourav <kjgourav@gmail.com>
2025-02-16 23:50:53 +0530

    the third

--- backend/utils/jwtTokens.js
+++ backend/utils/jwtTokens.js
@@ -1,2 +1,16 @@
 //to generate cookies or tokes for auth
 
+export const generateToken = (user,message,statusCode,res)=>{
+    const token = user.generateJsonWebToken();
+    const cookieName = user.role === "Admin" ? "adminToken" : "patientToken" ;
+    res.status(statusCode).cookie(cookieName , token,{
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE *24*60*60*1000
+    ),
+    }).json({
+        success:true,
+        message,
+        user,
+        token,
+
+    });
+};
