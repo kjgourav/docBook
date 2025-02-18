23b2b1495bcb29a0efbbe95293633e38b8b3c9ee
kjgourav <kjgourav@gmail.com>
2025-02-18 16:32:28 +0530

    4th commit

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,6 +2,9 @@ import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { User } from "../models/userSchema.js";
 import { generateToken } from "../utils/jwtTokens.js";
+import cloudinary from "cloudinary";
+
+
 
 export const patientRegister = catchAsyncErrors(async (req, res, next) => {
   const {
@@ -93,9 +96,11 @@ export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
   ) {
     return next(new ErrorHandler("please fill full form!", 400));
   }
-  const isRegistered = await User.findOne({email});
-  if(isRegistered){
-    return next(new ErrorHandler(`${isRegistered.role} With This Email Already Exists!`)); 
+  const isRegistered = await User.findOne({ email });
+  if (isRegistered) {
+    return next(
+      new ErrorHandler(`${isRegistered.role} With This Email Already Exists!`)
+    );
   }
   const admin = await User.create({
     firstName,
@@ -106,11 +111,120 @@ export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
     gender,
     dob,
     aadhaar,
-    role:"Admin",
+    role: "Admin"
   });
   res.status(200).json({
-    success:true,
-    message:"New Admin Registered!",
+    success: true,
+    message: "New Admin Registered!"
   });
+});
 
+export const getAllDoctors = catchAsyncErrors(async (req, res, next) => {
+  const doctors = await User.find({ role: "Doctor" });
+  res.status(200).json({
+    success: true,
+    doctors,
+  });
+});
+
+
+export const getUserDetails = catchAsyncErrors(async(req,res,next)=>{
+  const user = req.user;
+  res.status(200).json({
+    success:true,
+    user,
+  });
+});
+
+export const logoutAdmin = catchAsyncErrors(async(req,res,next)=>{
+  res
+  .status(200)
+  .cookie("adminToken","",{
+    httpOnly : true,
+    expires: new Date(Date.now()),
+  })
+  .json({
+    success:true,
+    message:"User Logout Succesfully!",
+  });
+});
+
+export const logoutPatient = catchAsyncErrors(async(req,res,next)=>{
+  res
+  .status(200)
+  .cookie("patientToken","",{
+    httpOnly : true,
+    expires: new Date(Date.now()),
+  })
+  .json({
+    success:true,
+    message:"User Logout Succesfully!",
+  });
+});
+
+export const addNewDoctor = catchAsyncErrors(async(req,res,next)=>{
+  if(!req.files || Object.keys(req.files).length === 0){
+    return next(new ErrorHandler("Doctor Avatar Required!",400));
+  }
+  const {docAvatar} = req.files;
+  const allowedFormats = ["image/png","image/jpeg","image/webp"];
+  if(!allowedFormats.includes(docAvatar.mimetype)){
+    return next(new ErrorHandler("File Format Not Supported",400));
+  }
+  const {  firstName,
+    lastName,
+    email,
+    phone,
+    password,
+    gender,
+    dob,
+    aadhaar,
+    doctorDepartment,
+    } = req.body;
+    if(
+      !firstName ||
+      !lastName||
+      !email||
+      !phone||
+      !password||
+      !gender||
+      !dob||
+      !aadhaar||
+      !doctorDepartment
+    ){
+      return next(new ErrorHandler("Please Provide Full Details!",400));
+    }
+    const isRegistered = await User.findOne({email});
+    if(isRegistered){
+      return next(new ErrorHandler(`${isRegistered.role} already registered with this email`,400));
+
+    }
+    const cloudinaryResponse = await cloudinary.uploader.upload(
+      docAvatar.tempFilePath
+    );
+    if(!cloudinaryResponse || cloudinaryResponse.error){
+      console.error("Cloudinary Error:",cloudinaryResponse.error || "Unknown Cloudinary Error");
+
+    }
+    const doctor = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    password,
+    gender,
+    dob,
+    aadhaar,
+    doctorDepartment,
+    role:"Doctor",
+    docAvatar:{
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    },
+    });
+    res.status(200).json({
+      success:true,
+      message:"New Doctor Registered!",
+      doctor
+    });
 });
